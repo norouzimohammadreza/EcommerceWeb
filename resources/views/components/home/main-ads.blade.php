aa4cdd1419f52fe8568f1a3f59af4857e7bea6cc
Mohammadreza Norouzi <rda0044@gmail.com>
2024-09-15 10:37:48 +0330

    adding ads banner in view of top page

---
+++ resources/views/components/home/main-ads.blade.php
@@ -0,0 +1,9 @@
+<section class="mb-3">
+    <section class="container-xxl">
+        <!-- one column -->
+        <section class="row py-4">
+            <section class="col"><img class="d-block rounded-2 w-100" src="{{asset('app/assets/images/ads/one-col-1.jpg')}}" alt=""></section>
+        </section>
+
+    </section>
+</section>
---
+++ resources/views/components/home/top-ads.blade.php
@@ -0,0 +1,10 @@
+<section class="mb-3">
+    <section class="container-xxl">
+        <!-- two column-->
+        <section class="row py-4">
+            <section class="col-12 col-md-6 mt-2 mt-md-0"><img class="d-block rounded-2 w-100" src="{{asset('app/assets/images/ads/two-col-1.jpg')}}" alt=""></section>
+            <section class="col-12 col-md-6 mt-2 mt-md-0"><img class="d-block rounded-2 w-100" src="{{asset('app/assets/images/ads/two-col-2.jpg')}}" alt=""></section>
+        </section>
+
+    </section>
+</section>
