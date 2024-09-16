4effdd66fdc86e2b1bd8fb759690938478beb83f
Mohammadreza Norouzi <rda0044@gmail.com>
2024-09-16 16:25:42 +0330

    add order items table

---
+++ database/migrations/2024_09_16_125016_create_order__items_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('order__items', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('order_id');
+            $table->foreign('order_id')->references('id')->on('orders');
+            $table->unsignedBigInteger('product_id');
+            $table->foreign('product_id')->references('id')->on('products');
+            $table->integer('quantity');
+            $table->unsignedBigInteger('price');
+            $table->unsignedBigInteger('total_price');
+            $table->text('product_object')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('order__items');
+    }
+};
