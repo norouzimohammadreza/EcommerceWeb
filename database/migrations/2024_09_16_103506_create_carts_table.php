ed57531fb72032d9485fa4d944c4c407ec0b5cb5
Mohammadreza Norouzi <rda0044@gmail.com>
2024-09-16 14:25:08 +0330

    add cart items tables for paying

---
+++ database/migrations/2024_09_16_103506_create_carts_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('carts', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('user_id');
+            $table->foreign('user_id')->references('id')->on('users');
+            $table->unsignedBigInteger('discount_id');
+            $table->foreign('discount_id')->references('id')->on('discounts');
+            $table->tinyInteger('status')->default(1)->comment('0 => close, 1 => open');
+            $table->timestamp('expired_at')->nullable();
+            $table->unsignedBigInteger('total_price');
+            $table->unsignedBigInteger('total_off_price')->nullable();
+            $table->tinyInteger('discount_status')->default(0)->comment('0 => unused, 1 => used');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('carts');
+    }
+};
