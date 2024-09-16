0371e4cce186bf7428c96b94e53b1b907d3ff129
Mohammadreza Norouzi <rda0044@gmail.com>
2024-09-16 12:04:36 +0330

    add deliveries table for delivering orders

---
+++ database/migrations/2024_09_16_083134_create_deliveries_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('deliveries', function (Blueprint $table) {
+            $table->id();
+            $table->string('name',191);
+            $table->string('time',191);
+            $table->string('unit',191);
+            $table->unsignedBigInteger('amount')->default(0);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('deliveries');
+    }
+};
